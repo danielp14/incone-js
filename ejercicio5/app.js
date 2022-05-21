493cb72e207036874959b62ce339cd4e06e9a106
Daniel Pintos <danielpintostec@gmail.com>
2022-05-21 12:33:39 -0300

    Archivos iniciales

---
+++ ejercicio5/app.js
@@ -0,0 +1,31 @@
+let alumnos = ['pepito','fulano','daniel','chanchito','mengano','murcielagosssss'];
+
+//console.log(alumnos[0]);
+//console.log(alumnos[alumnos.length-1]);
+
+/*
+alumnos.forEach(function(elemento, indice, array){
+    //console.log(elemento, indice);
+    if(elemento !== 'Daniel' && elemento.length == 10) {
+        console.log(`Encontramos a ${elemento} y su largo es ${elemento.length}`);
+    } else{
+        console.log(elemento);
+    }
+});*/
+/*let jugadores = [1,3,5,10,44,15];
+    nombres = ['carlos', 'pedro', 'antonio']
+console.log(nombres.sort());
+console.log(nombres.pop());
+console.log(nombres);
+nombres.push('Messi');
+console.log(nombres);
+nombres.unshift('Ronaldo');
+console.log(nombres);*/
+
+let usuario = {};
+    usuario.id = 876123786123678;
+    usuario.playlist = 87817231;
+    usuario.genero = 'country';
+    usuario.avatar = 'img/avatar.jpg';
+
+console.log(usuario);
