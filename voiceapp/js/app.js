71f54adfde038a177f5e4759b688f0a564ea4c27
Daniel Pintos <danielpintostec@gmail.com>
2022-07-09 16:55:02 -0300

    agregando app de voz

---
+++ voiceapp/js/app.js
@@ -0,0 +1,19 @@
+( function () {
+    self = this;
+    // logica de eventos
+    self.events = {
+        initApplication: function () {
+            console.log('arranco...');
+        }
+    }
+    //logica de las vistas
+    self.views = {
+        
+    }
+    
+    self.methods = {
+
+    }
+
+    self.events.initApplication();
+}())
